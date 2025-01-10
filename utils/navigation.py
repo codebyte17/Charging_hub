b90f0976dd1b784f5de9dc5a9c3c6a13b126e456
Hemant <hds45625@gmail.com>
2025-01-11 00:09:02 +0100

    Update code with file and folder structure

--- utils/navigation.py
+++
@@ -1,9 +0,0 @@
-# utils/navigation.py
-
-import streamlit as st
-
-def display_navigation():
-    st.sidebar.header("Navigation")
-    menu = ["Home", "Search by Postal Code"]
-    choice = st.sidebar.selectbox("Go to", menu)
-    return choice
